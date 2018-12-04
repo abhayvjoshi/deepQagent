f909d1f2d814c2bfb8a68cd535131505ae2b56a5
Shreyas <shosahal@asu.edu>
2018-12-03 23:37:39 -0700

    fix(getmatrix): Changing getMatrix function

--- deepQAgents.py
+++ deepQAgents.py
@@ -6,70 +6,35 @@ from collections import deque
 import sys
 import time
 import game
-
-params = {
-    # Model backups
-    'load_file': None,
-    'save_file': None,
-    'save_interval' : 10000,
-
-    # Training parameters
-    'train_start': 5000,    # Episodes before training starts
-    'batch_size': 32,       # Replay memory batch size
-    'mem_size': 100000,     # Replay memory size
-
-    'discount': 0.95,       # Discount rate (gamma value)
-    'lr': .002,            # Learning reate
-    # 'rms_decay': 0.99,      # RMS Prop decay (switched to adam)
-    # 'rms_eps': 1e-6,        # RMS Prop epsilon (switched to adam)
-
-    # Epsilon value (epsilon-greedy)
-    'eps': 1.0,             # Epsilon start value
-    'eps_final': 0.1,       # Epsilon end value
-    'eps_step': 10000       # Epsilon steps between start and end (linear)
-}
+import json
 
 
 
 class deepQAgents(game.Agent):
     def __init__(self, args):
-
-        print("Initialise DQN Agent")
-
-        # Load parameters from user-given arguments
-        self.params = params
+        with open('default_config.json') as f:
+            self.params = json.load(f)
         self.params['width'] = args['width']
         self.params['height'] = args['height']
         self.params['num_training'] = args['numTraining']
-
-        # Start Tensorflow session
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.1)
         self.sess = tf.Session(config = tf.ConfigProto(gpu_options = gpu_options))
         self.qnet = DQN(self.params)
-
-        # time started
         self.general_record_time = time.strftime("%a_%d_%b_%Y_%H_%M_%S", time.localtime())
-        # Q and cost
         self.Q_global = []
         self.cost_disp = 0
-
-        # Stats
         self.cnt = self.qnet.sess.run(self.qnet.global_step)
         self.local_cnt = 0
-
         self.numeps = 0
         self.last_score = 0
         self.s = time.time()
         self.last_reward = 0.
-
         self.replay_mem = deque()
         self.last_scores = deque()
 
 
     def getMove(self, state):
-        # Exploit / Explore
         if np.random.rand() > self.params['eps']:
-            # Exploit action
             self.Q_pred = self.qnet.sess.run(
                 self.qnet.y,
                 feed_dict = {self.qnet.x: np.reshape(self.current_state,
@@ -89,76 +54,54 @@ class deepQAgents(game.Agent):
                 move = self.get_direction(
                     a_winner[0][0])
         else:
-            # Random:
             move = self.get_direction(np.random.randint(0, 4))
-
-        # Save last_action
         self.last_action = self.get_value(move)
-
         return move
 
     def get_value(self, direction):
         if direction == Directions.NORTH:
-            return 0.
-        elif direction == Directions.EAST:
-            return 1.
+            return 0
         elif direction == Directions.SOUTH:
-            return 2.
+            return 1
+        elif direction == Directions.EAST:
+            return 2
         else:
-            return 3.
+            return 3
 
     def get_direction(self, value):
-        if value == 0.:
+        if value == 0:
             return Directions.NORTH
-        elif value == 1.:
-            return Directions.EAST
-        elif value == 2.:
+        elif value == 1:
             return Directions.SOUTH
+        elif value == 2:
+            return Directions.EAST
         else:
             return Directions.WEST
 
     def observation_step(self, state):
         if self.last_action is not None:
-            # Process current experience state
             self.last_state = np.copy(self.current_state)
             self.current_state = self.getStateMatrices(state)
-
-            # Process current experience reward
             self.current_score = state.getScore()
             reward = self.current_score - self.last_score
             self.last_score = self.current_score
-
             if reward > 20:
-                self.last_reward = 50.    # Eat ghost   (Yum! Yum!)
+                self.last_reward = 50.
             elif reward > 0:
-                self.last_reward = 10.    # Eat food    (Yum!)
+                self.last_reward = 10.
             elif reward < -10:
-                self.last_reward = -500.  # Get eaten   (Ouch!) -500
+                self.last_reward = -500.
                 self.won = False
             elif reward < 0:
-                self.last_reward = -1.    # Punish time (Pff..)
-
-
+                self.last_reward = -1.
             if(self.terminal and self.won):
                 self.last_reward = 100.
             self.ep_rew += self.last_reward
-
-            # Store last experience into memory
             experience = (self.last_state, float(self.last_reward), self.last_action, self.current_state, self.terminal)
             self.replay_mem.append(experience)
             if len(self.replay_mem) > self.params['mem_size']:
                 self.replay_mem.popleft()
-
-            # Save model
-            if(params['save_file']):
-                if self.local_cnt > self.params['train_start'] and self.local_cnt % self.params['save_interval'] == 0:
-                    self.qnet.save_ckpt('saves/model-' + params['save_file'] + "_" + str(self.cnt) + '_' + str(self.numeps))
-                    print('Model saved')
-
-            # Train
             self.train()
-
-        # Next
         self.local_cnt += 1
         self.frame += 1
         self.params['eps'] = max(self.params['eps_final'],
@@ -166,34 +109,26 @@ class deepQAgents(game.Agent):
 
 
     def observationFunction(self, state):
-        # Do observation
         self.terminal = False
         self.observation_step(state)
-
         return state
 
     def final(self, state):
-        # Next
         self.ep_rew += self.last_reward
-
-        # Do observation
         self.terminal = True
         self.observation_step(state)
-
         sys.stdout.write("# %4d | steps: %5d | steps_t: %5d | t: %4f | r: %12f | e: %10f \n" %
                          (self.numeps,self.local_cnt, self.cnt, time.time()-self.s, self.ep_rew, self.params['eps']))
         sys.stdout.flush()
 
     def train(self):
-        # Train
         if (self.local_cnt > self.params['train_start']):
             batch = random.sample(self.replay_mem, self.params['batch_size'])
-            batch_s = [] # States (s)
-            batch_r = [] # Rewards (r)
-            batch_a = [] # Actions (a)
-            batch_n = [] # Next states (s')
-            batch_t = [] # Terminal state (t)
-
+            batch_s = []
+            batch_r = []
+            batch_a = []
+            batch_n = []
+            batch_t = []
             for i in batch:
                 batch_s.append(i[0])
                 batch_r.append(i[1])
@@ -205,19 +140,16 @@ class deepQAgents(game.Agent):
             batch_a = self.get_onehot(np.array(batch_a))
             batch_n = np.array(batch_n)
             batch_t = np.array(batch_t)
-
             self.cnt, self.cost_disp = self.qnet.train(batch_s, batch_a, batch_t, batch_n, batch_r)
 
 
     def get_onehot(self, actions):
-        """ Create list of vectors with 1 values at index of action in list """
         actions_onehot = np.zeros((self.params['batch_size'], 4))
         for i in range(len(actions)):
             actions_onehot[i][int(actions[i])] = 1
         return actions_onehot
 
     def mergeStateMatrices(self, stateMatrices):
-        """ Merge state matrices to one state tensor """
         stateMatrices = np.swapaxes(stateMatrices, 0, 2)
         total = np.zeros((7, 7))
         for i in range(len(stateMatrices)):
@@ -225,136 +157,53 @@ class deepQAgents(game.Agent):
         return total
 
     def getStateMatrices(self, state):
-        """ Return wall, ghosts, food, capsules matrices """
-        def getWallMatrix(state):
-            """ Return matrix with wall coordinates set to 1 """
-            width, height = state.data.layout.width, state.data.layout.height
-            grid = state.data.layout.walls
-            matrix = np.zeros((height, width), dtype=np.int8)
-            for i in range(grid.height):
-                for j in range(grid.width):
-                    # Put cell vertically reversed in matrix
-                    cell = 1 if grid[j][i] else 0
-                    matrix[-1-i][j] = cell
-            return matrix
-
-        def getPacmanMatrix(state):
-            """ Return matrix with pacman coordinates set to 1 """
-            width, height = state.data.layout.width, state.data.layout.height
-            matrix = np.zeros((height, width), dtype=np.int8)
-
-            for agentState in state.data.agentStates:
-                if agentState.isPacman:
-                    pos = agentState.configuration.getPosition()
-                    cell = 1
-                    matrix[-1-int(pos[1])][int(pos[0])] = cell
-
-            return matrix
-
-        def getGhostMatrix(state):
-            """ Return matrix with ghost coordinates set to 1 """
-            width, height = state.data.layout.width, state.data.layout.height
-            matrix = np.zeros((height, width), dtype=np.int8)
-
-            for agentState in state.data.agentStates:
-                if not agentState.isPacman:
-                    if not agentState.scaredTimer > 0:
-                        pos = agentState.configuration.getPosition()
-                        cell = 1
-                        matrix[-1-int(pos[1])][int(pos[0])] = cell
-
-            return matrix
-
-        def getScaredGhostMatrix(state):
-            """ Return matrix with ghost coordinates set to 1 """
-            width, height = state.data.layout.width, state.data.layout.height
-            matrix = np.zeros((height, width), dtype=np.int8)
-
-            for agentState in state.data.agentStates:
-                if not agentState.isPacman:
-                    if agentState.scaredTimer > 0:
-                        pos = agentState.configuration.getPosition()
-                        cell = 1
-                        matrix[-1-int(pos[1])][int(pos[0])] = cell
-
-            return matrix
-
-        def getFoodMatrix(state):
-            """ Return matrix with food coordinates set to 1 """
-            width, height = state.data.layout.width, state.data.layout.height
-            grid = state.data.food
-            matrix = np.zeros((height, width), dtype=np.int8)
-
-            for i in range(grid.height):
-                for j in range(grid.width):
-                    # Put cell vertically reversed in matrix
-                    cell = 1 if grid[j][i] else 0
-                    matrix[-1-i][j] = cell
-
-            return matrix
-
-        def getCapsulesMatrix(state):
-            """ Return matrix with capsule coordinates set to 1 """
-            width, height = state.data.layout.width, state.data.layout.height
-            capsules = state.data.layout.capsules
-            matrix = np.zeros((height, width), dtype=np.int8)
-
-            for i in capsules:
-                # Insert capsule cells vertically reversed into matrix
-                matrix[-1-i[1], i[0]] = 1
-
-            return matrix
-
-        # Create observation matrix as a combination of
-        # wall, pacman, ghost, food and capsule matrices
-        # width, height = state.data.layout.width, state.data.layout.height
         width, height = self.params['width'], self.params['height']
-        observation = np.zeros((6, height, width))
+        input_matrix = np.zeros((6, width, height))
+        pacman_matrix = np.zeros((width, height))
+        ghost_matrix = np.zeros((width, height))
+        scared_matrix = np.zeros((width, height))
+        capsule_matrix = np.zeros((width, height))
+        input_matrix[0] = np.array(state.data.layout.walls.data, int)
+        for i in state.data.agentStates:
+            if i.isPacman:
+                pacman_pos = i.getPosition()
+                pacman_matrix[int(pacman_pos[0])][int(pacman_pos[1])] = 1
+            else:
+                if not i.scaredTimer > 0:
+                    ghost_pos = i.getPosition()
+                    ghost_matrix[int(ghost_pos[0])][int(ghost_pos[1])] = 1
+                else:
+                    scared_ghost_pos = i.getPosition()
+                    scared_matrix[int(scared_ghost_pos[0])][int(scared_ghost_pos[1])] = 1
+        input_matrix[1] = pacman_matrix
+        input_matrix[2] = ghost_matrix
+        input_matrix[3] = scared_matrix
+        input_matrix[4] = np.array(state.data.food.data, int)
+        for i in state.data.capsules:
+            capsule_matrix[i[0]][i[1]] = 1
+        input_matrix[5] = capsule_matrix
+        return np.swapaxes(input_matrix, 0, 2)
 
-        observation[0] = getWallMatrix(state)
-        observation[1] = getPacmanMatrix(state)
-        observation[2] = getGhostMatrix(state)
-        observation[3] = getScaredGhostMatrix(state)
-        observation[4] = getFoodMatrix(state)
-        observation[5] = getCapsulesMatrix(state)
-
-        observation = np.swapaxes(observation, 0, 2)
-
-        return observation
-
-    def registerInitialState(self, state): # inspects the starting state
-
-        # Reset reward
+    def registerInitialState(self, state):
         self.last_score = 0
         self.current_score = 0
         self.last_reward = 0.
         self.ep_rew = 0
-
-        # Reset state
         self.last_state = None
         self.current_state = self.getStateMatrices(state)
-
-        # Reset actions
         self.last_action = None
-
-        # Reset vars
         self.terminal = None
         self.won = True
         self.Q_global = []
         self.delay = 0
-
-        # Next
         self.frame = 0
         self.numeps += 1
 
     def getAction(self, state):
         move = self.getMove(state)
-
-        # Stop moving when not legal
         legal = state.getLegalActions(0)
         if move not in legal:
             move = Directions.STOP
-
         return move
 
 class DQN:
@@ -416,20 +265,14 @@ class DQN:
         self.Q_pred = tf.reduce_sum(tf.multiply(self.y,self.actions), reduction_indices=1)
         self.cost = tf.reduce_sum(tf.pow(tf.subtract(self.yj, self.Q_pred), 2))
 
-        if self.params['load_file'] is not None:
-            self.global_step = tf.Variable(int(self.params['load_file'].split('_')[-1]),name='global_step', trainable=False)
-        else:
-            self.global_step = tf.Variable(0, name='global_step', trainable=False)
+
+        self.global_step = tf.Variable(0, name='global_step', trainable=False)
 
         # self.optim = tf.train.RMSPropOptimizer(self.params['lr'],self.params['rms_decay'],0.0,self.params['rms_eps']).minimize(self.cost,global_step=self.global_step)
         self.optim = tf.train.AdamOptimizer(self.params['lr']).minimize(self.cost, global_step=self.global_step)
         self.saver = tf.train.Saver(max_to_keep=0)
 
         self.sess.run(tf.global_variables_initializer())
-
-        if self.params['load_file'] is not None:
-            print('Loading checkpoint...')
-            self.saver.restore(self.sess,self.params['load_file'])
 
 
     def train(self,bat_s,bat_a,bat_t,bat_n,bat_r):
@@ -439,6 +282,3 @@ class DQN:
         feed_dict={self.x: bat_s, self.q_t: q_t, self.actions: bat_a, self.terminals:bat_t, self.rewards: bat_r}
         _,cnt,cost = self.sess.run([self.optim, self.global_step,self.cost],feed_dict=feed_dict)
         return cnt, cost
-
-    def save_ckpt(self,filename):
-        self.saver.save(self.sess, filename)
